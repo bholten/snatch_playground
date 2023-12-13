8cd2cb603ad40fc18be4ba29bf34bd7406d9b6fc
Brennan Holten <bholten@protonmail.ch>
2023-12-13 09:00:05 -0600

    Update with current tests

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
-FROM erlang:23
+FROM erlang:22.3.4.26
+
+RUN apt-get update
+RUN apt-get -y install autoconf automake devscripts gawk g++ git-core git libexpat-dev
+RUN apt-get -y install ffmpeg libshout3-dev libmpg123-dev libmp3lame-dev
+RUN apt-get -y install dnsutils vim ngrep less net-tools
+RUN apt-get -y install libsox-dev libmp3lame0
+RUN apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 WORKDIR /usr/src/app
 
 COPY . .
 
 ENV PATH=$PATH:/root/.cache/rebar3/bin
-
 RUN rebar3 escriptize
 
 CMD ["_build/default/bin/snatch_playground"]
--- src/snatch_playground.erl
+++ src/snatch_playground.erl
@@ -1,43 +1,51 @@
 -module(snatch_playground).
 -export([main/1]).
 
+-include_lib("erlcloud/include/erlcloud_aws.hrl").
+
 -define(TEST_MESSAGE, <<"<iq id=\"test-bot\" to=\"alice@localhost\" from=\"bob@localhost/pc\" type=\"get\"><query/></iq>">>).
--define(TEST_ID, <<"LocalSnsQueue">>).
+-define(TEST_SNS_TOPIC, <<"arn:aws:sns:us-east-1:000000000000:LocalSNSTopic">>).
+-define(TEST_SQS_QUEUE, <<"http://localhost:4566/000000000000/LocalSQSQueue">>).
 
 main(_Args) ->
-    % Args = init:get_plain_arguments(), %% TODO
-    % ParsedConfig = parse_args(Args),
-    ok = application:ensure_all_started(snatch),
-    % {ok, SqsPid} = snatch:start_link(claws_xmpp, claws_aws_sqs, ParsedConfig),
-    SnsPid = start_sns_claw_localstack(),
+    {ok, _} = application:ensure_all_started(erlcloud),
+    {ok, _} = application:ensure_all_started(snatch),
+    % SnsPid = start_claw(sns),
+    % SqsPid = start_claw(sqs),
+    SnsPid = start_claw(aws),
+    SqsPid = start_claw(aws),
 
-    SnsPid ! {send, ?TEST_MESSAGE, ?TEST_ID},
+    % ok = claws_aws_sns:send(?TEST_MESSAGE, ?TEST_SNS_TOPIC),
+    % ok = claws_aws_sqs:send(?TEST_MESSAGE, ?TEST_SQS_QUEUE),
 
-    % ok = gen_server:stop(SqsPid),
     ok = gen_server:stop(SnsPid),
+    ok = gen_server:stop(SqsPid),
     ok = application:stop(snatch),
     halt().
 
-parse_args(Args) ->
-    lists:foldl(fun parse_arg/2, #{}, Args).
-
-parse_arg(Arg, Acc) ->
-    case string:split(Arg, "=", all) of
-        [Key, Value] ->
-            AtomKey = list_to_atom(string:trim(Key)),
-            maps:put(AtomKey, string:trim(Value), Acc);
-        _ ->
-            Acc
-    end.
-
-start_sns_claw_localstack() ->
-    DefaultConfig = #{
-        access_key_id => "foo",
-        secret_access_key => "bar",
-        sns_host => "localhost",
-        sns_scheme => "https://",
-        sns_port => "4566"
+start_claw(sns) ->
+    AwsConfig = #aws_config{
+        access_key_id = "test",
+        secret_access_key = "test",
+        aws_region = "us-east-1",
+        sns_host = "localhost",
+        sns_scheme = "http://",
+        sns_port = 4566
     },
-    {ok, Pid} = claws_aws_sns:start_link(DefaultConfig),
-    Pid.
+    {ok, Pid} = claws_aws_sns:start_link(AwsConfig),
+    Pid;
 
+start_claw(sqs) ->
+    AwsConfig = #aws_config{
+        access_key_id = "test",
+        secret_access_key = "test"
+        % sqs_host = "localhost"
+        % sqs_protocol = "http://",
+        % sqs_port = 4566
+    },
+    {ok, Pid} = claws_aws_sqs:start_link(AwsConfig),
+    Pid;
+
+start_claw(aws) ->
+    {ok, Pid} = claws_aws_sns:start_link(),
+    Pid.
